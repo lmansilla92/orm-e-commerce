6b5cf6c858d89e2c3c4dadd109ea626931dc266e
Luis Mansilla <l_mansilla92@yahoo.com>
2023-11-05 15:57:53 -0600

    Added route to delete category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -61,11 +61,25 @@ router.put('/:id', async (req, res) => {
     }
   } catch (err) {
     res.status(500).json(err);
-  }
+  };
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (categoryData){
+      res.status(200).json(categoryData);
+    } else {
+      res.status(404).json({ message: "No category with this id found" })
+    };
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
