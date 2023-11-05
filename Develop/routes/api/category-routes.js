e1edcf0a1590fbfaf8c971c5a1c107e846cdc0aa
Luis Mansilla <l_mansilla92@yahoo.com>
2023-11-05 15:20:43 -0600

    Added create new category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -36,8 +36,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+      // create a new category
+      const categoryData = await Category.create(req.body);
+      res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
